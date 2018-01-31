da4efacf379380c21a18cf97cfa0f7aeadcfe610
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-01-31 15:26:50 -0700

    first check point

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: "javascript: the Good Parts" },
+        { title: "javascript: the bad Parts" },
+        { title: "javascript: the cool Parts" },
+        { title: "javascript: the weird Parts" },
+        { title: "javascript: the yeeeee Parts" }
+    ];
+}
